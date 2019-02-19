28ce431bcfd6bdb6667e311efd38da7b56159092
afonyayoung <xafonya@gmail.com>
2019-02-19 13:48:29 +0300

    done

--- src/index.js
+++ src/index.js
@@ -4,4 +4,13 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+    var sum = 0;
+    for (var i=1; i <= preferences.length; i++){
+        var k = preferences[i-1];
+        var l = preferences[k-1]
+        if (preferences[i-1]===k && preferences[k-1]===l && preferences[l-1]===i){
+            sum++;
+        }
+    }
+    return(Math.floor(sum/3));
 };
